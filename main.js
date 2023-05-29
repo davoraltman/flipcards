db724f5951908dd573417c978f5ebbc5707f6f01
Davor Altman <davor.altman@gmail.com>
2023-05-29 22:18:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function displayRandomBoxElements() {
     const boxElements = document.querySelectorAll('.box');
     const shuffledElements = [...boxElements].sort(() => Math.random() - 0.5);
   
-    shuffledElements.forEach((element, index) => {
+   return shuffledElements.forEach((element, index) => {
       element.style.order = index;
       element.style.display = 'block'; // If elements are initially hidden, this line will display them
     });
